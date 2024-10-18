f31ff37a3585c2464dd95bf374fb1ceebcf8791c
okkhoy <okkhoy@gmail.com>
2024-10-18 16:04:08 +0800

    Code in class

--- src/main/java/org/utils/MyUtils.java
+++ src/main/java/org/utils/MyUtils.java
@@ -3,7 +3,6 @@ package org.utils;
 import org.utils.exceptions.InvalidStringInputException;
 import org.utils.stringutils.StringParser;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MyUtils {
@@ -11,9 +10,16 @@ public class MyUtils {
     public static void main(String[] args) {
         StringParser sp = new StringParser();
 
-        String str = "Hello|World|How are|you all";
+        String str = "Hello|World|How are|you all"; // | is a pipe
 
-        List<String> stringParts = sp.splitString(str, "\\|");
+        List<String> stringParts = null;
+        try {
+            stringParts = sp.splitString(str, "\\|");
+        } catch (InvalidStringInputException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Null encountered. Exception occurred.");
+        }
+        // using \\ means you are escaping the following character
 
         for (String part : stringParts) {
             System.out.println(part);
---
+++ src/test/java/org/utils/stringutils/StringParserTest.java
@@ -0,0 +1,34 @@
+package org.utils.stringutils;
+
+import org.junit.jupiter.api.Test;
+import org.utils.exceptions.InvalidStringInputException;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringParserTest {
+
+    // Test method naming
+    // methodBeingTested_input_expectedOutcome
+    @Test
+    void splitString_oneSeparator_expectTwoParts() throws InvalidStringInputException {  // happy path
+        StringParser stringParser = new StringParser();
+        String inputString = "hello|world";
+        String separator = "\\|";
+        List<String> stringParts = stringParser.splitString(inputString, separator);
+
+        assertEquals(2, stringParts.size());
+    }
+
+    // splitString_nullInput_expectException
+    @Test
+    void splitString_nullInput_expectException() throws InvalidStringInputException {
+        StringParser stringParser = new StringParser();
+        String inputString = null;
+        String separator = "\\|";
+//        List<String> stringParts = stringParser.splitString(inputString, separator);
+//        assertEquals(0, stringParts.size());
+        assertThrows(InvalidStringInputException.class,
+                () -> stringParser.splitString(inputString,separator));
+    }
+}
